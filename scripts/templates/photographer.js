ebcb81da0e90ab1d52df5e87670456e22f286873
thusy <web.dev418@gmail.com>
2024-02-19 14:54:45 +0100

    save

--- scripts/templates/photographer.js
+++ scripts/templates/photographer.js
@@ -1,7 +1,6 @@
 function photographerTemplate(data) {
-    const { name, portrait } = data;
-
-    const picture = `assets/photographers/${portrait}`;
+    const { name, portrait, city, country, tagline, price } = data;
+    const picture = `../../project_6/Front-End-Fisheye-main/assets/photographers/${portrait}`;
 
     function getUserCardDOM() {
         const article = document.createElement( 'article' );
@@ -9,8 +8,17 @@ function photographerTemplate(data) {
         img.setAttribute("src", picture)
         const h2 = document.createElement( 'h2' );
         h2.textContent = name;
+        const h3 = document.createElement( 'h3' );
+        h3.textContent = city + ', ' + country;
+        const p = document.createElement( 'p' );
+        p.textContent = tagline;
+        const span = document.createElement( 'span' );
+        span.textContent = price + '€/jour';
         article.appendChild(img);
         article.appendChild(h2);
+        article.appendChild(h3);
+        article.appendChild(p);
+        article.appendChild(span);
         return (article);
     }
     return { name, picture, getUserCardDOM }
